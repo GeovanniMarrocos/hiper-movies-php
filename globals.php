644df777683b29f7d4573d4c1306bccf945c581b
GeovanniMarrocos <geovanni.marrocos@gmail.com>
2025-08-01 20:22:27 -0300

    update files header

--- globals.php
+++ globals.php
@@ -1,3 +1,30 @@
 <?php 
-session_start();
-$BASE_URL = "http://" . $_SERVER["SERVER_NAME"] . dirname($_SERVER["REQUEST_URI"]. "?");
+// session_start();
+
+// $BASE_URL = "http://" . $_SERVER["SERVER_NAME"] . dirname($_SERVER["REQUEST_URI"]. "?");
+?>
+
+<?php
+// Inicia a sessão, se ainda não estiver iniciada
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+// 1. Detecta o protocolo (http ou https)
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https://" : "http://";
+
+// 2. Pega o nome do host (ex: localhost ou www.meusite.com.br)
+$host = $_SERVER['HTTP_HOST'];
+
+// 3. Pega o caminho do diretório do script atual de forma segura
+$script_name = $_SERVER['SCRIPT_NAME'];
+$project_path = dirname($script_name);
+
+// 4. Garante que o caminho termine com uma barra se não for o diretório raiz
+if ($project_path === '/' || $project_path === '\\') {
+    $project_path = ''; // Se estiver na raiz, não precisa de caminho extra
+}
+$project_path .= '/';
+
+// 5. Monta a URL base final e a define como uma constante
+define('BASE_URL', $protocol . $host . $project_path);
--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,7 @@
 <?php 
 require_once("globals.php");
 require_once("db.php");
+require_once("vendor/autoload.php");
 $flassMessage = [];
 
 ?>
@@ -14,13 +15,13 @@ $flassMessage = [];
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-  <link rel="shortcut icon" href="<?php $BASE_URL ?>img/favicon.svg" type="image/x-icon">
+  <link rel="shortcut icon" href="<?php BASE_URL ?>img/favicon.svg" type="image/x-icon">
   <!-- Bootstrap -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.7/css/bootstrap.css" integrity="sha512-Dg29JTs/r029HFd/aOkPcgmeELzRHukL99WqC7FPC+oyF4DClbMLlQANt5tXI1sgjpBGbcQIRqR4YNjI2LbNeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- CSS do projeto -->
-  <link rel="stylesheet" href="<?php $BASE_URL ?>./css/style.css">
+  <link rel="stylesheet" href="<?php BASE_URL ?>./css/style.css">
 
 </head>
 
@@ -28,16 +29,15 @@ $flassMessage = [];
   <header>
     <nav id="main-navbar" class="navbar navbar-expand-lg">
       <div class="logo-container d-flex">
-        <a href="<?php $BASE_URL ?>/hiper-movies/" class="navbar-brand d-flex">
-          <img class="img-logo" src="<?php $BASE_URL ?>./img/logo-dark.svg" alt="" srcset="">
+        <a href="<?php BASE_URL ?>/hiper-movies/" class="navbar-brand d-flex">
+          <img class="img-logo" src="<?php BASE_URL ?>./img/logo-dark.svg" alt="" srcset="">
         </a>
-
       </div>
-      <button class=" navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle Navigation">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle Navigation">
         <i class="fas fa-bars"></i>
       </button>
       <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
-        <input type="text" name="q" id="search" class=" form-control mr-sm-2" type="search" placeholder="BUSCAR FILMES" aria-label="Search">
+        <input type="text" name="q" id="search" class="form-control mr-sm-2 font-1-s " type="search" placeholder="BUSCAR FILMES" aria-label="Search">
         <button class="btn my-2 my-sm-0" type="submit">
           <i class="fas fa-search"></i>
         </button>
@@ -45,7 +45,7 @@ $flassMessage = [];
       <div class="collapse navbar-collapse" id="navbar">
         <ul class="navbar nav">
           <li class="nav-item">
-            <a href="<?php $BASE_URL ?>auth.php" class="nav-link">Entrar / Cadastrar</a>
+            <a href="<?php BASE_URL ?>auth.php" class="nav-link font-1-s cor-0">Entrar - Cadastrar</a>
           </li>
         </ul>
       </div>
@@ -55,5 +55,5 @@ $flassMessage = [];
 
   <?php endif;?>
   <div class="msg-container">
-    <p class="msg">Testando Mensagem</p>
+    <p class="msg font-1-xs text-uppercase">Testando Mensagem</p>
   </div>
